905692eb47689088760eb5b904e23328ddb835a8
Keyur Patel <keyur.holodox@gmail.com>
2020-02-19 01:19:09 +0530

    subfield missing

---
+++ src/app/model/SubField.ts
@@ -0,0 +1,12 @@
+import { FormControl } from "@angular/forms";
+
+export class SubField {
+ 
+    name: string;
+    placeholder: string;
+    visible: boolean;
+    frmControl: FormControl;
+
+    
+    
+}
